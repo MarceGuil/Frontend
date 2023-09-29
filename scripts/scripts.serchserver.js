87de42dd219b21c2d51b136cb5efceee10d12603
MarceGuil <143463053+MarceGuil@users.noreply.github.com>
2023-09-28 23:12:05 -0300

    Update scripts.serchserver.js

--- scripts/scripts.serchserver.js
+++ scripts/scripts.serchserver.js
@@ -18,26 +18,6 @@ document.addEventListener("DOMContentLoaded", function() {
         };
     }
 
-    // Obtén una referencia al elemento <ul> donde se mostrarán los servidores
-    const serverList = document.getElementById("server-list");
-
-    // Lista de servidores a los que se ha unido el usuario (EJEMPLO)
-    const serversJoined = ["Servidor 1", "Servidor 2", "Servidor 3"];
-
-    // Recorre la lista de servidores y crea elementos <li>
-    serversJoined.forEach((server) => {
-        // Crea un elemento <li>
-        const listItem = document.createElement("li");
-
-        // Establece el contenido de <li> como el nombre del servidor
-        listItem.textContent = server;
-
-        // Agrega el <li> a la lista de servidores
-        if (serverList) {
-            serverList.appendChild(listItem);
-        }
-    });
-
     // Agrega eventos para habilitar la edición al hacer clic en los botones "Editar"
     var editButtons = document.querySelectorAll(".edit-button");
     editButtons.forEach((button) => {
